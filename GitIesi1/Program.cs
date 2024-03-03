333ad465a8488b6e2315716cfa6bcb97850d25a3
Ardan Dordziev <ardandor120306@gail.com>
2024-03-03 21:28:09 +0800

    first commit

---
+++ GitIesi1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitIesi1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{a} {b}");
+        }
+    }
+}
