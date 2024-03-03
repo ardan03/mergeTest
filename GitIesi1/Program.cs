d277d3584d5d2962454c02fb28de1c5d78fc246c
Ardan Dordziev <ardandor120306@gail.com>
2024-03-03 22:22:59 +0800

    metod addition

--- GitIesi1/Program.cs
+++ GitIesi1/Program.cs
@@ -8,12 +8,16 @@ namespace GitIesi1
 {
     internal class Program
     {
+        public static int addition(int a,int b)
+        {
+            return a+ b;
+        }
         static void Main(string[] args)
         {
             int a, b;
             a = int.Parse(Console.ReadLine());
             b = int.Parse(Console.ReadLine());
-            Console.WriteLine($"{a} {b}");
+            Console.WriteLine(addition(a,b));
         }
     }
 }
